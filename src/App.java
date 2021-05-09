4ea35ece5947d76d64face6250be686b410ca27c
mohamed ahmed <mohamed.ahmedmoreb@gmail.com>
2021-05-10 00:29:30 +0200

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
 import java.util.*;
+
+import javax.lang.model.util.Elements;
+
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
@@ -21,8 +27,8 @@ public class App {
         // Retrieving a collection
         MongoCollection<Document> collection = database.getCollection("Search-Engine");
         System.out.println("Collection Search-Engine selected successfully");
-        String text="";
-        String URL="";
+        String text = "";
+        String URL = "";
         Document document = new Document("text", text).append("URL", URL);
 
         // Inserting document into the collection
@@ -47,13 +53,14 @@ public class App {
 class webCrawler implements Runnable {
 
     public int Num;
-// hashiing
-private HashSet<String> links;
+    // hashiing
+    private HashSet<String> links;
 
-public webCrawler( int n) {
-    Num =n;
-    links = new HashSet<String>();
-}
+    public webCrawler(int n) {
+        Num = n;
+        links = new HashSet<String>();
+    }
+
     public void run() {
         for (int i = 0; i < Num; i++) {
             if (Integer.parseInt(Thread.currentThread().getName()) == i) {
@@ -66,28 +73,29 @@ public webCrawler( int n) {
     }
 
     public void getPageLinks(String URL) {
-        //4. Check if you have already crawled the URLs 
-        //(we are intentionally not checking for duplicate content in this example)
+        // 4. Check if you have already crawled the URLs
+        // (we are intentionally not checking for duplicate content in this example)
         if (!links.contains(URL)) {
             try {
-                //4. (i) If not add it to the index
+                // 4. (i) If not add it to the index
                 if (links.add(URL)) {
                     System.out.println(URL);
                 }
 
-                //2. Fetch the HTML code
-                Document document = Jsoup.connect(URL).get();
-                //3. Parse the HTML to extract links to other URLs
-                Elements linksOnPage = document.select("a[href]");
+                // 2. Fetch the HTML code
+                org.jsoup.nodes.Document document = Jsoup.connect(URL).get();
+                // 3. Parse the HTML to extract links to other URLs
+                org.jsoup.select.Elements linksOnPage = document.select("a[href]");
 
-                //5. For each extracted URL... go back to Step 4.
+                // 5. For each extracted URL... go back to Step 4.
                 for (Element page : linksOnPage) {
                     getPageLinks(page.attr("abs:href"));
                 }
             } catch (IOException e) {
                 System.err.println("For '" + URL + "': " + e.getMessage());
             }
-        
-    }
 
+        }
+
+    }
 }
